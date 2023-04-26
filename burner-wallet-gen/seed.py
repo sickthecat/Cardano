02826cb59111f0ac2f20da6c9ea8ffd7ef58c88d
Sick the Cat <129430216+sickthecat@users.noreply.github.com>
2023-04-26 12:35:22 -0400

    Update seed.py

--- burner-wallet-gen/seed.py
+++ burner-wallet-gen/seed.py
@@ -3,10 +3,25 @@ from mnemonic import Mnemonic
 # Prompt user for number of seed phrases to generate
 num_seed_phrases = int(input("Enter the number of seed phrases to generate: "))
 
-# Generate the specified number of 24-word seed phrases and save to file
+# Prompt user for desired seed phrase length
+print("Select the desired seed phrase length:")
+print("1. 12 words")
+print("2. 15 words")
+print("3. 24 words")
+length_option = int(input("Enter your choice (1, 2, or 3): "))
+
+# Generate the specified number of seed phrases and save to file
 with open('seedphrases.txt', 'w') as f:
     for i in range(num_seed_phrases):
-        entropy = Mnemonic("english").generate(strength=256)  # generate 256 bits of entropy
+        if length_option == 1:
+            entropy = Mnemonic("english").generate(strength=128)
+        elif length_option == 2:
+            entropy = Mnemonic("english").generate(strength=160)
+        elif length_option == 3:
+            entropy = Mnemonic("english").generate(strength=256)
+        else:
+            print(f"Invalid seed phrase length option selected: {length_option}")
+            break
         seed_phrase = entropy.split()
         formatted_seed_phrase = ' '.join(seed_phrase)
         f.write(f"Seed Phrase #{i+1}: {formatted_seed_phrase}\n")
